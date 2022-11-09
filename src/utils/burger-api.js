cb6c706cdeb709a672274f8a61a581d80a331a50
Alexey Yanchev <yanchevalexey@yandex.ru>
2022-11-09 13:51:23 +0300

    исправил ошибку в названии функции

--- src/utils/burger-api.js
+++ src/utils/burger-api.js
@@ -1,6 +1,6 @@
 import { URL_FOR_GET_DATA } from "./constants";
-import { checkReponse } from "./utils";
+import { checkResponse } from "./utils";
 
 export function getIngredients() {
-  return fetch(`${URL_FOR_GET_DATA}/ingredients`).then(checkReponse);
+  return fetch(`${URL_FOR_GET_DATA}/ingredients`).then(checkResponse);
 }
