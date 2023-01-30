35e2c6db62ff78d9bd88763e096b1020bf2ddf9b
Alexey Yanchev <yanchevalexey@yandex.ru>
2023-01-30 23:53:14 +0300

    исправил ошибку в wsMiddleware. Ненужные диспатчи проходили через него

--- src/services/middlewares/wsMiddleware.js
+++ src/services/middlewares/wsMiddleware.js
@@ -9,49 +9,50 @@ export const socketMiddleware = () => {
         wsUrl = action.wsUrl;
         wsActions = action.wsActions;
       }
-      const { dispatch, getState } = store;
-      const { type, payload } = action;
-      const { wsInit, wsSendMessage, onOpen, onClose, onError, onMessage } =
-        wsActions;
+      if (wsUrl && wsActions) {
+        const { dispatch, getState } = store;
+        const { type, payload } = action;
+        const { wsInit, wsSendMessage, onOpen, onClose, onError, onMessage } =
+          wsActions;
 
-      if (type === wsInit) {
-        socket = new WebSocket(wsUrl);
+        if (type === wsInit) {
+          socket = new WebSocket(wsUrl);
+        }
+        const { user } = getState().userReducer;
+        if (type === wsInit && user) {
+          const token = user.accessToken.split("Bearer ")[1];
+          socket = new WebSocket(`${wsUrl}?token=${token}`);
+        }
+        if (socket) {
+          socket.onopen = (event) => {
+            dispatch({ type: onOpen, payload: event });
+          };
+
+          socket.onerror = (event) => {
+            dispatch({ type: onError, payload: event });
+          };
+
+          socket.onmessage = (event) => {
+            const { data } = event;
+            if (data === "ping") {
+              socket.send("pong");
+            }
+            const parsedData = JSON.parse(data);
+            const { success, ...restParsedData } = parsedData;
+            dispatch({ type: onMessage, payload: restParsedData });
+          };
+
+          socket.onclose = (event) => {
+            dispatch({ type: onClose, payload: event });
+          };
+
+          // if (type === wsSendMessage) {
+          //   const message = payload;
+          //   message.token = user.accessToken;
+          //   socket.send(JSON.stringify(message));
+          // }
+        }
       }
-      const { user } = getState().userReducer;
-      if (type === wsInit && user) {
-        const token = user.accessToken.split("Bearer ")[1];
-        socket = new WebSocket(`${wsUrl}?token=${token}`);
-      }
-      if (socket) {
-        socket.onopen = (event) => {
-          dispatch({ type: onOpen, payload: event });
-        };
-
-        socket.onerror = (event) => {
-          dispatch({ type: onError, payload: event });
-        };
-
-        socket.onmessage = (event) => {
-          const { data } = event;
-          if (data === "ping") {
-            socket.send("pong");
-          }
-          const parsedData = JSON.parse(data);
-          const { success, ...restParsedData } = parsedData;
-          dispatch({ type: onMessage, payload: restParsedData });
-        };
-
-        socket.onclose = (event) => {
-          dispatch({ type: onClose, payload: event });
-        };
-
-        // if (type === wsSendMessage) {
-        //   const message = payload;
-        //   message.token = user.accessToken;
-        //   socket.send(JSON.stringify(message));
-        // }
-      }
-
       next(action);
     };
   };
